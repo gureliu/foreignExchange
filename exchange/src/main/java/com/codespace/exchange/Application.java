4cc38375ba6383df132622da469e30baf0c2bf54
ugureli <ugureli@INFNBW125.infina.com.tr>
2020-01-16 23:16:46 +0300

    initial commit

--- exchange/src/main/java/com/codespace/exchange/Application.java
+++ exchange/src/main/java/com/codespace/exchange/Application.java
@@ -8,10 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Main
- * 
  * @author ugureli
- *
  */
 @SpringBootApplication
 public class Application {
